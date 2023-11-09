88715c46655dfe5b194650fa9a74c86dc39a5873
k-kumar-01 <kushalkumargupta4@gmail.com>
2023-11-10 04:25:43 +0530

    feat: add drag drop component to upload video
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

---
+++ app/components/DragDrop/index.tsx
@@ -0,0 +1,93 @@
+import { useRef, useState } from "react";
+import styles from "./index.module.css";
+
+type DragDropFileProps = {
+  setFile: any
+};
+
+const DragDropFile = ({ setFile }: DragDropFileProps) => {
+  const [dragActive, setDragActive] = useState(false);
+  // ref
+  const inputRef = useRef<any>(null);
+
+  // handle drag events
+  const handleDrag = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
+
+  // triggers when file is dropped
+  const handleDrop = function (e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer!.files && e.dataTransfer!.files[0]) {
+      if (e.dataTransfer!.files[0].type === "video/mp4") {
+        console.log(e.dataTransfer.files[0], e.dataTransfer);
+        setFile(e.dataTransfer.files[0]);
+      } else {
+        alert("Only mp4 video formats are supported");
+      }
+    }
+  };
+
+  // triggers when file is selected with click
+  const handleChange = function (e: any) {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  // triggers the input when the button is clicked
+  const onButtonClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  return (
+    <form
+      id={styles["form-file-upload"]}
+      onDragEnter={handleDrag}
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <input
+        ref={inputRef}
+        type="file"
+        id={styles["input-file-upload"]}
+        multiple={true}
+        onChange={handleChange}
+        accept="video/mp4"
+      />
+      <label
+        id={styles["label-file-upload"]}
+        htmlFor={styles["input-file-upload"]}
+        className={dragActive ? styles["drag-active"] : ""}
+      >
+        <div>
+          <p>Drag and drop your file here or</p>
+          <button className="upload-button" onClick={onButtonClick}>
+            Upload a file
+          </button>
+        </div>
+      </label>
+      {dragActive && (
+        <div
+          id={styles["drag-file-element"]}
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDrag}
+          onDrop={handleDrop}
+        ></div>
+      )}
+    </form>
+  );
+};
+
+export default DragDropFile;
